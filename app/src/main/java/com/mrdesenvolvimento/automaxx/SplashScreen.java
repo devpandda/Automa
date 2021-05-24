f59b565dacfcaa3e07fb3b6fd52657a1a3e25588
dev.pandda@gmail.com <$Regiane10*>
2021-05-24 01:20:57 -0300

    layout bg_splash

--- app/src/main/java/com/mrdesenvolvimento/automaxx/SplashScreen.java
+++ app/src/main/java/com/mrdesenvolvimento/automaxx/SplashScreen.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 5000;
+    public ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        pb = (ProgressBar) findViewById(R.id.splashProgress);
+
+        pb.setIndeterminate(true);
 
         new Handler().postDelayed(new Runnable() {
             @Override
